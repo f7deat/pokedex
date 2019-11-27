5953bb579c71fdb66a8fd14315a4b7527cd5c236
Đinh Công Tân <tan.dct9x@gmail.com>
2019-11-27 19:47:55 +0700

    Sprite

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -16,12 +16,43 @@ export class PokemonComponent implements OnInit {
     ) {  }
 
     pokemon: any = [];
+    icon: string;
+    activeClass = 1;
     getPokemon(): void {
       this.pokemonService.getPokemon(this.route.snapshot.params['id']).subscribe(
         (x) => {
           this.pokemon = x;
         }
       );
+    }
+    changeSprite(spriteName: string): void {
+      if (spriteName === 'front_default') {
+        this.icon = this.pokemon.sprites.front_default;
+        this.activeClass = 1;
+      } else if (spriteName === 'back_default') {
+        this.icon = this.pokemon.sprites.back_default;
+        this.activeClass = 2;
+      } else if (spriteName === 'front_female') {
+        this.icon = this.pokemon.sprites.front_female;
+        this.activeClass = 3;
+      } else if (spriteName === 'back_female') {
+        this.icon = this.pokemon.sprites.back_female;
+        this.activeClass = 4;
+      } else if (spriteName === 'front_shiny') {
+        this.icon = this.pokemon.sprites.front_shiny;
+        this.activeClass = 5;
+      } else if (spriteName === 'back_shiny') {
+        this.icon = this.pokemon.sprites.back_shiny;
+        this.activeClass = 6;
+      } else if (spriteName === 'front_shiny_female') {
+        this.icon = this.pokemon.sprites.front_shiny_female;
+        this.activeClass = 7;
+      } else if (spriteName === 'back_shiny_female') {
+        this.icon = this.pokemon.sprites.back_shiny_female;
+        this.activeClass = 8;
+      } else {
+        this.icon = null;
+      }
     }
 
 
