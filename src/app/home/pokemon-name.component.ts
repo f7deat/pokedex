f21439ac1b85d03ec5a1c0d62df4d8f403c924fa
TanDC_VM <tandc_vm@novaon.dc>
2020-09-12 16:55:08 +0700

    fix icon path

--- src/app/home/pokemon-name.component.ts
+++ src/app/home/pokemon-name.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'pokemon-name',
-    templateUrl: './pokemon-name.component.html',
+    template: `<div class="icon {{type}} p-2 mr-2" style="width: 2rem;">
+                    <img src="assets/icons/{{type}}.svg"/>
+                  </div>`,
     styleUrls: []
   })
 export class PokemonNameComponent implements OnInit {
